a951ac9b5043ade3855abcae9e456aa95115b7bf
huypham0808 <phamngohoanghuy95@gmail.com>
2023-08-23 21:24:33 +0700

    Initial Project_Config Environment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//Import Bootstrap@4.3.1 & Jquery
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'jquery/dist/jquery.min.js'
+//
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,8 +15,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
