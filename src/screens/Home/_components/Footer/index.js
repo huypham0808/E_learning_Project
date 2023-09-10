79c7293c71f26a9d8be5c4404f3cec98cfbcb810
huypham0808 <phamngohoanghuy95@gmail.com>
2023-09-10 11:50:07 +0700

    [FE]: Fix Carousel

--- src/screens/Home/_components/Footer/index.js
+++ src/screens/Home/_components/Footer/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './style.css'
-//import { Link } from 'react-router-dom';
-//import InputformName from '../../../../components/Inputform';
+
 
 export default function Footer() {
     return (
