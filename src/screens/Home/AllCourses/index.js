85fe651734c38ab1d018471196731d48c74d28aa
Huydev0808 <phamngohoanghuy95@gmail.com>
2023-09-24 14:55:08 +0700

    [FE]: Add effect for Allcourse page

--- src/screens/Home/AllCourses/index.js
+++ src/screens/Home/AllCourses/index.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchListCourse } from '../../../redux/types/actions';
 import '../../../assets/style.css'
 import Loader from '../../../components/Loader';
+import Aos from 'aos';
 
 export default function Allcourse() {
   const dispatch = useDispatch();
@@ -16,10 +17,13 @@ export default function Allcourse() {
   const currentCourse = data?.slice(indexOfFirstCourse, indexOfLastCourse);
   useEffect(() => {
     dispatch(fetchListCourse());
-  },[]);
+  }, []);
 
+  useEffect(() => {
+    Aos.init({ duration: 3000 })
+  }, []);
   const renderCourse = () => {
-    if (loading) return <Loader/>;
+    if (loading) return <Loader />;
     return currentCourse?.map((course, index) => {
       return <CourseItem course={course} key={index} />
     })
@@ -49,7 +53,7 @@ export default function Allcourse() {
           <h2>Khóa học</h2>
           <p>Tất cả khóa học</p>
         </div>
-        <div className='row'>
+        <div className='row' data-aos="fade-up">
           {renderCourse()}
         </div>
         <ul className='pagination justify-content-center mt-4 '>
