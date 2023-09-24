10576d84b6f58d5c3614133baae880445020b281
Huydev0808 <phamngohoanghuy95@gmail.com>
2023-09-24 16:31:47 +0700

    [FE]: Implement admin page

--- src/screens/Admin/index.js
+++ src/screens/Admin/index.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Dashboard from './Dashboard'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import {actTryLogout} from '../../redux/types/actions'
 
 export default function Admin() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(()=> {
+    dispatch(actTryLogout(navigate));
+  },[])
   return (
     <div>
       <Dashboard/>
